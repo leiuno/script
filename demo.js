7dbb0c073a5ea9e33ecca00a1ca4299dcca15b83
leiuno <372159394@qq.com>
2018-05-05 10:55:43 +0800

    add demo.js

---
+++ demo.js
@@ -0,0 +1,17 @@
+
+
+var fs = require('fs')
+var dirName = process.argv[2]
+
+fs.mkdirSync("./" + dirName)
+process.chdir("./" + dirName)
+fs.mkdirSync('css')
+fs.mkdirSync('js')
+fs.writeFileSync("./index.html", 
+"<!DOCTYPE>" + "\r" +
+"<title>Hello</title>" + "\r" +
+"<h1>Hi</h1>")
+fs.writeFileSync("css/style.css", "h1{color: red;}")
+fs.writeFileSync("js/main.js","var string = 'Hello World'"+"\r"+
+"alert(string)")
+process.exit(0)
